b12f8b7acce8cbda71a7afaf4a7cca1b5a5e0e68
gruba <grubavijay@gmail.com>
2024-02-15 13:09:06 +0530

    changed password to passwords

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
                                 <input type="text" class="form-control" id="name" placeholder="Please Enter your FullName" required>
                             </div>
                             <div class="mb-3">
-                                <label for="">Password</label>
+                                <label for="">Passwords</label>
                                 <input type="password" class="form-control" id="Pass" placeholder="Please Enter your Password" required>
                             </div>
                         </div>
@@ -58,6 +58,6 @@
        
     </body>
     <script>
-        
+
     </script>
 </html>
